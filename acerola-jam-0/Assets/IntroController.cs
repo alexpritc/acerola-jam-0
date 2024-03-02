81e7f617f38c1b7c0dd0001a57fceaf518e3a494
Alex Pritchard <hi@apritc.com>
2024-03-02 17:34:28 +0000

    Add intro and intro skips

---
+++ acerola-jam-0/Assets/IntroController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class IntroController : MonoBehaviour
+{
+    private static Controls controller;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        controller = new Controls();
+        controller.Player.Space.performed += ctx => SkipAnim();
+    }
+
+    void SkipAnim()
+    {
+       //Fetch the current Animation clip information for the base layer
+       string clip = animator.GetCurrentAnimatorClipInfo(0)[0].clip.name;
+
+        switch (clip) {
+            default:
+                break;
+            case "friend-walk-in":
+                animator.Play("friend-intro");
+                break;
+            case "friend-intro-1":
+                animator.Play("friend-intro-2");
+                break;
+            case "friend-intro-2":
+                animator.Play("friend-intro-3");
+                break;
+            case "friend-intro-3":
+                animator.Play("friend_idle");
+                break;
+        }   
+
+    }
+
+    // Required for the input system.
+    void OnEnable()
+    {
+        controller.Player.Enable();
+    }
+
+    void OnDisable()
+    {
+        controller.Player.Disable();
+    }
+}
